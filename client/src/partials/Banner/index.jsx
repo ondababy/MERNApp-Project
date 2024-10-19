2f05416ad64f6db978b2219f8ba30fd6dcb54e5e
deJames-13 <de.james013@gmail.com>
2024-10-19 11:21:03 +0800

    asdf

--- client/src/partials/Banner/index.jsx
+++ client/src/partials/Banner/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 export function Banner() {
-    return (
-        <>
-            {/* BANNER */}
-            <div className='border border-blue-400 min-h-56'>
-            </div>
-        </>
-    )
+  return (
+    <>
+      {/* BANNER */}
+      <div className='border border-blue-400 min-h-56'>
+      </div>
+    </>
+  )
 }
--- client/src/screens/Shop/index.jsx
+++ client/src/screens/Shop/index.jsx
@@ -5,36 +5,34 @@ import { ProductFilters, ProductGrid } from '@features'
 import { Banner } from '@partials'
 
 export default function ShopScreen() {
-    return (
-        <div className="w-full">
-            {/* BANNER */}
-            <Banner />
+  return (
+    <div className="w-full">
+      {/* BANNER */}
+      <Banner />
 
-            {/* CONTENT */}
-            <div className='flex min-h-screen '>
-                {/* Filters */}
-                <ProductFilters />
-
-
-
-                {/* Products */}
-                <div className="flex flex-col mx-8 py-4 w-full">
-
-                    {/* Headline */}
-                    <div className="flex justify-between items-center">
-                        <h1 className="text-5xl font-display">
-                            Our Products
-                        </h1>
-                        <SelectComponent />
-                    </div>
-
-                    {/* Content */}
-                    <ProductGrid />
-
-                </div>
+      {/* CONTENT */}
+      <div className='flex min-h-screen '>
+        {/* Filters */}
+        <ProductFilters />
 
 
-            </div>
+
+        {/* Products */}
+        <div className="flex flex-col mx-8 py-4 w-full">
+
+          {/* Headline */}
+          <div className="flex justify-between items-center">
+            <h1 className="text-5xl font-display">
+              Our Products
+            </h1>
+            <SelectComponent />
+          </div>
+
+          {/* Content */}
+          <ProductGrid />
+
         </div>
-    )
+      </div>
+    </div>
+  )
 }
