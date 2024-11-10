5b3d9bb5053a385828216e6f45c3c41461c5219b
OndaBaby <139729721+OndaBaby@users.noreply.github.com>
2024-11-10 17:31:02 +0800

    reviews aayusin pa

--- client/src/features/index.js
+++ client/src/features/index.js
@@ -9,5 +9,4 @@ export * from './Users/index.js';
 export * from './Cart/index.js';
 export * from './Orders/index.js';
 export * from './Wishlist/index.js';
-export * from './Category/index.js';
 export * from './Categories/index.js';
