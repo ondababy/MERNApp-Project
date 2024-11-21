c967b631bb8a853532d7a2dda7d33904aef6987e
deJames-13 <de.james013@gmail.com>
2024-11-22 02:45:27 +0800

    s

--- client/src/features/Products/product.fields.js
+++ client/src/features/Products/product.fields.js
@@ -1,62 +1,3 @@
-// const getFields = () => [
-//   {
-//     label: 'Name',
-//     name: 'name',
-//     type: 'text',
-//     outerStyle: 'w-full col-span-3 row-span-2 md:col-span-1',
-//   },
-//   {
-//     label: 'Price',
-//     name: 'price',
-//     type: 'number',
-//     outerStyle: 'w-full col-span-3 row-span-2 md:col-span-1',
-//   },
-//   {
-//     label: 'Stock',
-//     name: 'stock',
-//     type: 'number',
-//     outerStyle: 'w-full col-span-3 row-span-2 md:col-span-1',
-//   },
-//   {
-//     label: 'Description',
-//     name: 'description',
-//     variant: 'textarea',
-//     outerStyle: 'w-full col-span-3 row-span-2',
-//     placeholder: 'Enter your product description.',
-//   },
-//   // {
-//   //   label: 'Brand',
-//   //   name: 'brand',
-//   //   variant: 'textarea',
-//   //   outerStyle: 'w-full col-span-3 row-span-2',
-//   //   placeholder: 'Enter your product description.',
-//   // },
-//   // {
-//   //   label: 'Supplier',
-//   //   name: 'supplier',
-//   //   variant: 'textarea',
-//   //   outerStyle: 'w-full col-span-3 row-span-2',
-//   //   placeholder: 'Enter your product description.',
-//   // },
-//   {
-//     label: 'Image',
-//     name: 'image',
-//     type: 'image',
-//     accept: 'image/*',
-//     multiple: true,
-//     outerStyle: 'w-full col-span-3 row-span-2',
-//   },
-//   // More getFields can be added here
-// ];
-
-// const getAltFields = () => [
-//   { label: 'Name', name: 'name', type: 'text' },
-//   // alternate getFields can be added here
-// ];
-
-// export { getAltFields, getFields };
-
-
 const getFields = () => [
   {
     label: 'Name',
@@ -83,20 +24,20 @@ const getFields = () => [
     outerStyle: 'w-full col-span-3 row-span-2',
     placeholder: 'Enter your product description.',
   },
-  {
-    label: 'Brand',
-    name: 'brand',
-    type: 'select', // Assuming it's a dropdown/select field
-    options: [], // This will be populated dynamically with API call
-    outerStyle: 'w-full col-span-3 row-span-2 md:col-span-1',
-  },
-  {
-    label: 'Supplier',
-    name: 'supplier',
-    type: 'select', // Assuming it's a dropdown/select field
-    options: [], // This will be populated dynamically with API call
-    outerStyle: 'w-full col-span-3 row-span-2 md:col-span-1',
-  },
+  // {
+  //   label: 'Brand',
+  //   name: 'brand',
+  //   variant: 'textarea',
+  //   outerStyle: 'w-full col-span-3 row-span-2',
+  //   placeholder: 'Enter your product description.',
+  // },
+  // {
+  //   label: 'Supplier',
+  //   name: 'supplier',
+  //   variant: 'textarea',
+  //   outerStyle: 'w-full col-span-3 row-span-2',
+  //   placeholder: 'Enter your product description.',
+  // },
   {
     label: 'Image',
     name: 'image',
@@ -105,10 +46,15 @@ const getFields = () => [
     multiple: true,
     outerStyle: 'w-full col-span-3 row-span-2',
   },
+  // More getFields can be added here
 ];
 
 const getAltFields = () => [
   { label: 'Name', name: 'name', type: 'text' },
+  // alternate getFields can be added here
 ];
 
 export { getAltFields, getFields };
+
+
+
