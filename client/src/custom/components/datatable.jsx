ee846921d32508a38a07cb11877108236f758d88
deJames-13 <de.james013@gmail.com>
2024-11-21 11:54:40 +0800

    producttable

--- client/src/custom/components/datatable.jsx
+++ client/src/custom/components/datatable.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import {
   flexRender,
@@ -7,12 +7,11 @@ import {
   getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
-} from "@tanstack/react-table"
-import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react"
-import * as React from "react"
+} from "@tanstack/react-table";
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react";
+import * as React from "react";
 
-import { Button } from "@common/components/ui/button"
-import { Checkbox } from "@common/components/ui/checkbox"
+import { Button } from "@common/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
@@ -21,8 +20,8 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@common/components/ui/dropdown-menu"
-import { Input } from "@common/components/ui/input"
+} from "@common/components/ui/dropdown-menu";
+import { Input } from "@common/components/ui/input";
 import {
   Table,
   TableBody,
@@ -30,139 +29,18 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@common/components/ui/table"
+} from "@common/components/ui/table";
 
-const data = [
-  {
-    id: "m5gr84i9",
-    amount: 316,
-    status: "success",
-    email: "ken99@yahoo.com",
-  },
-  {
-    id: "3u1reuv4",
-    amount: 242,
-    status: "success",
-    email: "Abe45@gmail.com",
-  },
-  {
-    id: "derv1ws0",
-    amount: 837,
-    status: "processing",
-    email: "Monserrat44@gmail.com",
-  },
-  {
-    id: "5kma53ae",
-    amount: 874,
-    status: "success",
-    email: "Silas22@gmail.com",
-  },
-  {
-    id: "bhqecj4p",
-    amount: 721,
-    status: "failed",
-    email: "carmella@hotmail.com",
-  },
-]
+export function DataTable({ data, columns }) {
+  if (!columns || !Array.isArray(columns) || columns.length === 0) {
+    return;
+  }
 
-
-export const columns = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-        className="cursor-pointer border border-primary"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-        className="cursor-pointer border border-primary"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    accessorKey: "status",
-    header: "Status",
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("status")}</div>
-    ),
-  },
-  {
-    accessorKey: "email",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Email
-          <ArrowUpDown />
-        </Button>
-      )
-    },
-    cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
-  },
-  {
-    accessorKey: "amount",
-    header: () => <div className="text-right">Amount</div>,
-    cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"))
-
-      const formatted = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(amount)
-
-      return <div className="text-right font-medium">{formatted}</div>
-    },
-  },
-  {
-    id: "actions",
-    enableHiding: false,
-    cell: ({ row }) => {
-      const payment = row.original
-
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      )
-    },
-  },
-]
-
-export function DataTable() {
-  const [sorting, setSorting] = React.useState([])
-  const [columnFilters, setColumnFilters] = React.useState([])
-  const [columnVisibility, setColumnVisibility] = React.useState({})
-  const [rowSelection, setRowSelection] = React.useState({})
+  const [sorting, setSorting] = React.useState([]);
+  const [columnFilters, setColumnFilters] = React.useState([]);
+  const [columnVisibility, setColumnVisibility] = React.useState({});
+  const [rowSelection, setRowSelection] = React.useState({});
+  const [selectedFilter, setSelectedFilter] = React.useState("");
 
   const table = useReactTable({
     data,
@@ -181,29 +59,70 @@ export function DataTable() {
       columnVisibility,
       rowSelection,
     },
-  })
+  });
 
   return (
     <div className="w-full text-base-content">
-
       <div className="flex items-center py-4">
-        <Input
-          placeholder="Filter emails..."
-          value={(table.getColumn("email")?.getFilterValue()) ?? ""}
-          onChange={(event) =>
-            table.getColumn("email")?.setFilterValue(event.target.value)
-          }
-          className="max-w-sm"
-        />
+        <div className="flex gap-2">
+          <Input
+            placeholder="Filter by..."
+            value={!selectedFilter ? '' : table.getColumn(selectedFilter)?.getFilterValue() ?? ""}
+            onChange={(event) =>
+              !selectedFilter ? '' : table.getColumn(selectedFilter)?.setFilterValue(`${event.target.value}`)
+            }
+            className="max-w-sm"
+          />
+          {/* Select Filter Dropdown */}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="outline"
+                className="ml-auto text-base-content bg-base-100 capitalize"
+              >
+                {selectedFilter || 'Filter'} <ChevronDown />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              align="end"
+              className="text-base-content bg-base-100"
+            >
+              {table
+                .getAllColumns()
+                .filter((column) => column.getCanFilter())
+                .map((column) => {
+                  if (column.excludeFromFilter) return;
+                  return (
+                    <DropdownMenuItem
+                      key={column.id}
+                      className="capitalize"
+                      onClick={() => setSelectedFilter(column.id)}
+                    >
+                      {column.id}
+                    </DropdownMenuItem>
+                  );
+                })}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
 
 
+
+
+        {/* Select Columns */}
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="ml-auto text-base-content bg-base-100">
+            <Button
+              variant="outline"
+              className="ml-auto text-base-content bg-base-100"
+            >
               Columns <ChevronDown />
             </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent align="end" className="text-base-content bg-base-100">
+          <DropdownMenuContent
+            align="end"
+            className="text-base-content bg-base-100"
+          >
             {table
               .getAllColumns()
               .filter((column) => column.getCanHide())
@@ -219,13 +138,15 @@ export function DataTable() {
                   >
                     {column.id}
                   </DropdownMenuCheckboxItem>
-                )
+                );
               })}
           </DropdownMenuContent>
         </DropdownMenu>
+
       </div>
 
 
+      {/* MAIN TABLE */}
       <div className="rounded-md border">
         <Table>
           <TableHeader>
@@ -241,7 +162,7 @@ export function DataTable() {
                           header.getContext()
                         )}
                     </TableHead>
-                  )
+                  );
                 })}
               </TableRow>
             ))}
@@ -255,7 +176,7 @@ export function DataTable() {
                   className={"hover:bg-base-content/10"}
                 >
                   {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id} >
+                    <TableCell key={cell.id}>
                       {flexRender(
                         cell.column.columnDef.cell,
                         cell.getContext()
@@ -266,10 +187,7 @@ export function DataTable() {
               ))
             ) : (
               <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
+                <TableCell colSpan={columns.length} className="h-24 text-center">
                   No results.
                 </TableCell>
               </TableRow>
@@ -279,13 +197,10 @@ export function DataTable() {
       </div>
 
       <div className="flex items-center justify-end space-x-2 py-4">
-
-
         <div className="flex-1 text-sm text-muted-foreground">
           {table.getFilteredSelectedRowModel().rows.length} of{" "}
           {table.getFilteredRowModel().rows.length} row(s) selected.
         </div>
-
 
         <div className="space-x-2">
           <Button
@@ -307,9 +222,7 @@ export function DataTable() {
             Next
           </Button>
         </div>
-
-
       </div>
     </div>
-  )
+  );
 }
--- client/src/features/Products/components/ProductTable.jsx
+++ client/src/features/Products/components/ProductTable.jsx
@@ -1,5 +1,7 @@
 import { ActionButtons, Table } from '@common';
+import { Checkbox } from "@common/components/ui/checkbox";
 import { confirmDelete, DataTable } from '@custom';
+import { ArrowUpDown } from "lucide-react";
 import { useEffect, useState } from 'react';
 import { Button } from 'react-daisyui';
 import { FaPlus } from 'react-icons/fa';
@@ -8,16 +10,92 @@ import { toast } from 'react-toastify';
 import { productApi } from '../product.api';
 import ProductWrapper from './ProductWrapper';
 
-const allowedColumns = () => [
-  { key: 'name', label: 'Name' },
-  { key: 'actions', label: '' },
-  // More columns can be added here
-];
+const makeData = (products, handleDelete) => {
+  return products.map((product) => ({
+    id: product.id,
+    name: product.name,
+    stock: `${product.stock}`,
+    price: `${product.price}`,
+    actions: { id: product.id, handleDelete, slug: product.slug },
+  }));
+};
+
+const makeColumns = (navigate) => {
+  const interactiveColumns = ["name", "stock", "price"].map((key) => {
+    return {
+      accessorKey: key,
+      header: ({ column }) => {
+        return (
+          <Button
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            className='bg-transparent border-none btn-sm capitalize'
+          >
+            {key}
+            <ArrowUpDown />
+          </Button>
+        );
+      },
+      cell: ({ row }) => <p className='break-all px-3'>{row.getValue(key)}</p>
+    }
+  });
+  return [
+    // CHECKBOX
+    {
+      id: "select",
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
+    // MINI IMAGE
+    {
+      accessorKey: 'image',
+      header: 'Image',
+      cell: <>image here</>,
+      enableColumnFilter: false,
+      enableSorting: false,
+
+    },
+    ...interactiveColumns,
+    {
+      accessorKey: 'actions',
+      header: 'Actions',
+      cell: ({ row }) => (
+        <ActionButtons
+          onDelete={() => row.getValue('actions').handleDelete(row.getValue('actions').id)}
+          onEdit={() => navigate(`/dashboard/products/${row.getValue('actions').slug}/edit`)}
+          onView={() => navigate(`/dashboard/products/${row.getValue('actions').slug}/view`)}
+        />
+      ),
+      enableColumnFilter: false,
+      enableSorting: false,
+    }
+  ]
+};
 
 const ProductTable = () => {
   const navigate = useNavigate();
   const { useGetProductsMutation, useDeleteProductMutation } = productApi;
   const [products, setProducts] = useState([]);
+  const [table, setTable] = useState({
+    data: null,
+    columns: null,
+  })
   const [getProducts, { isLoading, isError }] = useGetProductsMutation();
   const [deleteProduct, { isLoading: isDeleting }] = useDeleteProductMutation();
 
@@ -48,9 +126,16 @@ const ProductTable = () => {
     };
   }, [getProducts]);
 
+  useEffect(() => {
+    setTable({
+      data: makeData(products, handleDelete),
+      columns: makeColumns(navigate),
+    });
+  }, [products]);
+
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error loading products</div>;
-  if (!products.length)
+  if (!products.length) {
     return (
       <div className="flex items-center justify-center space-x-2 font-bold text-center">
         <span>No data found! Create one. </span>
@@ -63,26 +148,12 @@ const ProductTable = () => {
         </Button>
       </div>
     );
+  }
+
   return (
     <>
       <ProductWrapper title="Products Table">
-        {/* <Table
-          data={products.map((product) => ({
-            ...product,
-            actions: (
-              <ActionButtons
-                key={'action_' + product.slug}
-                className="flex justify-end"
-                isLoading={isDeleting}
-                onDelete={() => handleDelete(product.id)}
-                onEdit={() => navigate(`/dashboard/products/${product.slug}/edit`)}
-                onView={() => navigate(`/dashboard/products/${product.slug}/view`)}
-              />
-            ),
-          }))}
-          columns={allowedColumns()}
-        /> */}
-        <DataTable />
+        <DataTable {...table} />
       </ProductWrapper>
     </>
   );
