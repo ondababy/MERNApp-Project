4675cbebd366b864c84238103d49ebf42b90ee9c
deJames-13 <de.james013@gmail.com>
2024-11-25 13:53:08 +0800

    s

--- server/server.js
+++ server/server.js
@@ -38,8 +38,11 @@ const server = () => {
   app.use(error.notFound);
   app.use(error.errorMiddleware);
 
-  app.listen(PORT, () => {
+
+
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
+    console
   });
 };
 
