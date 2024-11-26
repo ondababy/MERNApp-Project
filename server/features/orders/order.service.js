812267af381783dc371d2afa6ca189eab9800951
deJames-13 <de.james013@gmail.com>
2024-11-26 12:58:48 +0800

    merge r

--- server/features/orders/order.service.js
+++ server/features/orders/order.service.js
@@ -54,7 +54,7 @@ class OrderService extends Service {
     
     const message = `Your order ${order.id} has been placed!`;
     const title = 'Order Confirmation';
-    const altMessage = this.makeAltMessage(data);
+    const altMessage = this.makeAltMessage(order);
 
     sendEmail({
       email: user.email,
